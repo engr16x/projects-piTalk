fd9d313a6df013a081cc42320987e63a74f3f867
trevordmeyer <35824308+trevordmeyer@users.noreply.github.com>
2018-07-08 21:58:04 -0400

    Add files via upload

--- setup.py
+++ setup.py
@@ -85,7 +85,7 @@ setup(
     #   py_modules=["my_module"],
     #
     #packages=find_packages(exclude=['contrib', 'docs', 'tests']),  # Required
-    py_modules=["piTalk"],
+    py_modules=["src/piTalk"],
     
 #    # This field lists other packages that your project depends on to run.
 #    # Any package you put here will be installed by pip when your project is
